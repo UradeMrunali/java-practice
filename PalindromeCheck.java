099d0c4d02d0b581ebe291a3b1e66d5f13c2a592
Mrunali <mrunaliurade26@gmail.com>
2025-07-24 12:19:06 +0530

    Checking number is Palindrome or not

---
+++ PalindromeCheck.java
@@ -0,0 +1,25 @@
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        int num = 122;
+
+        PalindromeCheck obj = new PalindromeCheck();
+        boolean pal=obj.isPalindrome(num);
+        if(pal){
+            System.out.println("Number is palindrome");
+        }else{
+            System.out.println("Not palindrome");
+        }
+    }
+
+
+    public boolean isPalindrome(int num) {
+        int original = num;
+        int reversed = 0;
+        while (num != 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num = num / 10;
+        }
+        return original == reversed;
+    }
+}
