69cd0c730c83ed4d5e0e53da29f1d45833a09f87
Mrunali <mrunaliurade26@gmail.com>
2025-07-25 09:53:48 +0530

    Multiplication table code

---
+++ MultiplicationTable.java
@@ -0,0 +1,14 @@
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        int num = 9;
+
+        printTable(num);
+    }
+
+    static void printTable (int num) {
+        System.out.println("Table of "+ num + ":");
+        for(int i = 1; i <= 10 ; i++){
+            System.out.println(num+ "*" + i + "=" + (num * i));
+        }
+    }
+}
