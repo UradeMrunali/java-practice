4284b1525c816f1d34b0b97d74ac2c0542d21d95
Mrunali <mrunaliurade26@gmail.com>
2025-07-25 07:55:24 +0530

    Swapping two numbers

---
+++ NumSwapper.java
@@ -0,0 +1,24 @@
+public class NumSwapper {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 200;
+
+        System.out.println("Initial Numbers:");
+        displayNumbers(a,b);
+
+        swapNumbers(a,b);
+
+    }
+
+    static void swapNumbers (int a, int b){
+        int c = a;
+        a = b;
+        b = c ;
+        System.out.println("Swaped Numbers: ");
+        System.out.println("a = "+ a + ", b = "+ b);
+    }
+
+   static void displayNumbers (int a, int b){
+        System.out.println("a = "+ a + ", b = "+ b);
+    }
+}
