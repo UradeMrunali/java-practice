cf62116da2da87d0af07b97208fbdf3adebccffb
Mrunali <mrunaliurade26@gmail.com>
2025-07-29 09:36:51 +0530

    Printing factorial of a number

---
+++ Factorial.java
@@ -0,0 +1,17 @@
+public class Factorial {
+    int printFactorial (int n){
+        int num = 1;
+        for (int i = 1 ; i <= n; i++) {
+            num = num * i;
+        }
+            return num;
+
+    }
+
+    public static void main(String[] args) {
+        Factorial a = new Factorial();
+        int n = 6;
+      int result=  a.printFactorial( n);
+        System.out.println("Factorial of " + n + " = " + result);
+    }
+}
