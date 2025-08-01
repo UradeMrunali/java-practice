157f9ed44aac88164ed78a848a758705dad97551
Mrunali <mrunaliurade26@gmail.com>
2025-08-01 15:25:09 +0530

    Program of checking exception handling

--- TryCatch.java
+++ TryCatch.java
@@ -3,10 +3,11 @@ public class TryCatch {
         try {
             int[] num = {1, 2, 3};
             System.out.println(num[8]);
-        } catch (ArrayIndexOutOfBoundsException e){
+        } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Error: You're trying to access an invalid index!");
         } finally {
             System.out.println("This is program of checking try catch and finally block");
 
+        }
     }
 }
