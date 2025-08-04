281a1228c778b019d1c8d1f0409ad799749595d5
Mrunali <mrunaliurade26@gmail.com>
2025-08-04 09:51:57 +0530

    ArmstrongCheck code

---
+++ ArmstrongCheck.java
@@ -0,0 +1,20 @@
+public class ArmstrongCheck {
+  static boolean isArmstrong (int num){
+        int original = num;
+        int sum = 0;
+        while(num > 0){
+            int digit = num % 10;
+            sum += digit * digit * digit ;
+            num /= 10;
+        }
+
+        return sum==original;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isArmstrong(153));
+        System.out.println(isArmstrong(122));
+        System.out.println(isArmstrong(370));
+
+    }
+}
