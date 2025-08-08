2294421efa456b9448769bfb69131152bad0916c
Mrunali <mrunaliurade26@gmail.com>
2025-08-08 07:21:31 +0530

    Working with integers in an ArrayList and performing mathematical operations.

---
+++ NumbersArrayList.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class NumbersArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+//        Add these numbers: 50, 70, 30, 90, 100, 60.
+//        Find and print:
+//        Maximum number
+//        Minimum number
+//        Average of numbers
+//        Remove all numbers less than 60.
+
+        numbers.add(50);
+        numbers.add(70);
+        numbers.add(30);
+        numbers.add(90);
+        numbers.add(100);
+        numbers.add(60);
+        System.out.println("Numbers in list are: " + numbers);
+
+        int max = Collections.max(numbers);
+        System.out.println("Maximum number is: " + max);
+
+        int min =  Collections.min(numbers);
+        System.out.println("Minimum number is: " + min);
+
+        int sum = 0;
+        for(int num : numbers){
+           sum += num ;
+        }
+        double average = (double) sum / numbers.size();
+        System.out.println("Average of the numbers in list is: " + average);
+
+        numbers.removeIf(n -> n < 60 );
+        System.out.println("After removing number less than 60 remaining list is: " + numbers);
+
+
+    }
+}
