6c96bbf359a2bca27e83a6a1815981f8329fd2f6
Mrunali <mrunaliurade26@gmail.com>
2025-08-09 09:53:22 +0530

    Sorting the list alphabetically.

---
+++ SortingArrayList.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SortingArrayList {
+    public static void main(String[] args) {
+//        Create an ArrayList of Strings named fruits.
+//                Add fruits: Apple, Banana, Orange, Mango, Kiwi.
+//                Sort the list alphabetically.
+//                Search for "Mango" and display its index.
+//        Check if "Papaya" is in the list.
+
+        ArrayList<String> fruits = new ArrayList<>();
+        Collections.addAll(fruits,"Apple", "Banana" , "Orange" , "Mango" , "Kiwi");
+        System.out.println("Fruits in the list are: "+ fruits);
+
+        Collections.sort(fruits);
+        System.out.println("Sorted list:" + fruits);
+
+        int mangoIndex = fruits.indexOf("Mango");
+        if(mangoIndex != -1){
+            System.out.println("Mango is at the index of: " + mangoIndex);
+        } else {
+            System.out.println("Mango is not in the list");
+        }
+
+        if(fruits.contains("Papaya")){
+            System.out.println("Yes Papaya is available in list");
+        }else {
+            System.out.println("No papaya is unavailable ");
+        }
+    }
+}
