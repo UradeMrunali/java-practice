393e1240e8dff800eabf5e145838c5fda608ae19
Mrunali <mrunaliurade26@gmail.com>
2025-08-18 12:34:18 +0530

    Leap Year checking

---
+++ LeapYear.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to New Year Calculator");
+        System.out.print("Enter the year you want to check: ");
+        int year= sc.nextInt();;
+
+        if(year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)){
+            System.out.println("This is Leap Year");
+        } else{
+            System.out.println("This is not a Leap Year");
+        }
+    }
+}
