1c7e71cc4b65ec9dd735f2f1e6b9a7a60367fb40
Mrunali Urade <124710812+UradeMrunali@users.noreply.github.com>
2025-09-08 15:53:01 +0530

    DBConnection.java

--- hospital-management-system/src/crud/DBConnection.java
+++ hospital-management-system/src/crud/DBConnection.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+
 public class DBConnection {
     private static final String URL = "jdbc:postgresql://localhost:5432/hospital-db";
     private static final String USER = "postgres";
