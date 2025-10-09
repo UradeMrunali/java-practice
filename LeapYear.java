7a150ff427bed3771974fb38c631441dd89ac913
Mrunali Urade <124710812+UradeMrunali@users.noreply.github.com>
2025-10-09 15:51:53 +0530

    Update LeapYear.java

--- LeapYear.java
+++ LeapYear.java
@@ -8,6 +8,8 @@ public class LeapYear {
         System.out.print("Enter the year you want to check: ");
         int year= sc.nextInt();
 
+
+        
         
         if(year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)){
             System.out.println("This is Leap Year");
