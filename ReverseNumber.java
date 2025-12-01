06857f4aa9796af8b03d52aaa29e53cd8d360312
Mrunali Urade <124710812+UradeMrunali@users.noreply.github.com>
2025-12-01 12:18:41 +0530

    Reverse number code

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -1,7 +1,6 @@
 public class ReverseNumber {
     public static int reverseNum(int num){
         int rev = 0;
-
         while (num > 0){
             rev = rev * 10 + num % 10;
             num /= 10;
