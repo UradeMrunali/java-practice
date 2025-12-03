4971840d6354ae1c66b601dff3f6660ebef67373
Mrunali Urade <124710812+UradeMrunali@users.noreply.github.com>
2025-12-03 11:03:24 +0530

    Leap Year checking

--- LeapYear.java
+++ LeapYear.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 public class LeapYear {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to New Year Calculator");
         System.out.print("Enter the year you want to check: ");
