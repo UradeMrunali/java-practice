da33b90451525ba9084fa5ec7eafd51592f8176b
Mrunali Urade <124710812+UradeMrunali@users.noreply.github.com>
2025-12-10 13:44:57 +0530

    Creating and manipulating a simple ArrayList

--- ColorArrayList.java
+++ ColorArrayList.java
@@ -7,6 +7,7 @@ public class ColorArrayList {
         colors.add("White");
         colors.add("Green");
         colors.add("Red");
+        
         System.out.println("All color in the list are: " + colors);
         colors.add(2,"Orange");
         System.out.println("After adding new color: " + colors);
