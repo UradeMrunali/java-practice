8059cc3c85d18440e5d26b29dc25d773170cd1ec
Mrunali Urade <124710812+UradeMrunali@users.noreply.github.com>
2025-12-18 14:05:23 +0530

    Domain pricing

--- DomainApplication.java
+++ DomainApplication.java
@@ -11,16 +11,14 @@ public class DomainApplication {
 //    M = Annual Markup Decrease (e.g., 5.00%)
 //    N = Minimum Markup (e.g., 15.00%)
 
-        //    I Markup Price (Strike Through) B * (1 + F)
+//    I Markup Price (Strike Through) B * (1 + F)
 //    K Final Consumer Price I - (I * J)
 //    L Price per Selected Years (Strike Through) (C * (1 + F)) * H
 //    O Markup for Selected Years max(F * (1 - M)^H, N)
 //    P Price Per Year Final C * (1 + O * J)
 //    Q Final Price with Renewals (C * (1 + O) * (1 - J)) * H
     DomainPriceCalculator.DomainPriceResult res = DomainPriceCalculator.calculatePrice
-            (100, 100,
-                    50, 0.50,
-                    100, 2, 0.10, 0.05, 0.15);
+            (100, 100, 50, 0.50, 100, 2, 0.10, 0.05, 0.15);
 
         System.out.println("Markup Price: " + res.markupPrice);
         System.out.println("Final Consumer Price: " + res.finalConsumerPrice);
@@ -28,7 +26,5 @@ public class DomainApplication {
        // System.out.println("Markup for Selected Years: "  );
         System.out.println("Price Per Year Final: " + res.pricePerFinalYear);
         System.out.println("Final Price with Renewals: " + res.finalPriceWithRenewals);
-
-        
     }
 }
