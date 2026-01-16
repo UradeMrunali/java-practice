e276800d06d46d41ea0eef7a8fbd892fe5fb4018
Mrunali Urade <124710812+UradeMrunali@users.noreply.github.com>
2026-01-16 13:19:18 +0530

    Multiplication table code

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -3,6 +3,8 @@ public class MultiplicationTable {
         int num = 9;
         printTable(num);
     }
+
+    
     static void printTable (int num) {
         System.out.println("Table of "+ num + ":");
         for(int i = 1; i <= 10 ; i++){
