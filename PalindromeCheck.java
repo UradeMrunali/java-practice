f2e7c978b6712be1d3801b23feec07cc4faaad86
Mrunali Urade <124710812+UradeMrunali@users.noreply.github.com>
2026-02-07 16:26:10 +0530

    Checking number is Palindrome or not

--- PalindromeCheck.java
+++ PalindromeCheck.java
@@ -8,9 +8,7 @@ public class PalindromeCheck {
         }else{
             System.out.println("Not palindrome");
         }
-    }    
-
-    
+    }   
     public boolean isPalindrome(int num) {
         int original = num;
         int reversed = 0;
